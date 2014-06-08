d2f52ddef494a650a2f8ebcffb288dd4a27e07e8
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-08 13:02:55 +0200

    Ported over more classes from ORRE

---
+++ OpenRR/src/openrr/world/properties/ChrystalAppearance.java
@@ -0,0 +1,34 @@
+package openrr.world.properties;
+
+import orre.gameWorld.core.GameObject;
+import orre.gameWorld.core.Message;
+import orre.gameWorld.core.PropertyType;
+import orre.gameWorld.properties.Appearance;
+
+public class ChrystalAppearance extends Appearance {
+
+	public ChrystalAppearance(GameObject gameObject) {
+		super(PropertyType.CHRYSTAL_APPEARANCE, "chrystal", gameObject);
+	}
+
+	@Override
+	public void handleMessage(Message<?> message) {
+		
+	}
+
+	@Override
+	public void tick() {
+		
+	}
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	protected void initAppearance() {
+		
+	}
+
+}
---
+++ OpenRR/src/openrr/world/properties/OreAppearance.java
@@ -0,0 +1,33 @@
+package openrr.world.properties;
+
+import orre.gameWorld.core.GameObject;
+import orre.gameWorld.core.Message;
+import orre.gameWorld.core.PropertyType;
+import orre.gameWorld.properties.Appearance;
+
+public class OreAppearance extends Appearance {
+	public OreAppearance(GameObject gameObject) {
+		super(PropertyType.ORE_APPEARANCE, "ore", gameObject);
+	}
+
+	@Override
+	public void handleMessage(Message<?> message) {
+
+	}
+
+	@Override
+	public void tick() {
+
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	protected void initAppearance() {
+		this.appearance.root.setLocation(48.5, 50.5, 0);
+	}
+
+}
