f6a94a91b13846d06d7a1289b5d41941ad045f0c
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-11 15:04:09 +0200

    Sync commit

--- OpenRR/src/openrr/OpenRR.java
+++ OpenRR/src/openrr/OpenRR.java
@@ -7,7 +7,8 @@ import orre.api.ORRE;
 public class OpenRR {
 	public static void main(String[] args) {
 		OpenRRPropertyProvider provider = new OpenRRPropertyProvider();
-		ORRE engine = ORRE.init(args, new GameSettings("Terrain", provider));
+		GameSettings settings = new GameSettings("Terrain", provider);
+		ORRE engine = ORRE.init(args, settings);
 		engine.registerResourceLoader(new MapLoader(), new String[]{".rrm"});
 		engine.run();
 	}
