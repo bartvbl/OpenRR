4905ff6e47a6c768f2356980e466bce075fd2c33
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-17 00:24:37 +0200

    Spawning some sample objects on to the scene

--- OpenRR/res/scripts/main.py
+++ OpenRR/res/scripts/main.py
@@ -2,5 +2,7 @@ def buildMainScene(eventParam):
 	if eventParam == 'GAME_RUNNING':
 		spawn('MAP')
 		spawn('MAP_CONTROLLER')
+		spawn('TOOL_STORE')
+		spawn('ROCK_RAIDER')
 	
 on('gameStateChanged', buildMainScene)
