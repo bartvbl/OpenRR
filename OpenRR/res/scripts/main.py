341afb221c988c681b4bd3c0722fc3fa468d19dc
Bart van Blokland <bartiver@stud.ntnu.no>
2014-06-24 15:16:03 +0200

    Generalised code to switch between side menus

--- OpenRR/res/scripts/main.py
+++ OpenRR/res/scripts/main.py
@@ -12,12 +12,39 @@ def buildMainScene(params):
 	gui.show('topMainMenu')
 	gui.show('resourceSidebar')
 	gui.show('sideMainMenu')
-	gui.show('buildMenu')
 		
-@on('GUI_Click', action='switchToBuildingMenu')
-def switchToBuildingMenu(eventParam):
-	gui.animateMenu('sideMainMenu', 'hideSidebar')
+orr_activeSideMenu = 'sideMainMenu'
+
+def registerSideMenuSwitch(actionName, menuName):
+	@on('GUI_Click', action=actionName)
+	def switchToBuildingMenu(eventParam):
+		global orr_activeSideMenu
+		orr_activeSideMenu = menuName
+		gui.animateMenu('sideMainMenu', 'hideSidebar')
+		
+	@on('GUI_AnimationComplete', menuName=menuName)
+	def swapMenus(eventParams):
+		global orr_activeSideMenu
+		if orr_activeSideMenu != 'sideMainMenu':
+			return
+		gui.show('sideMainMenu')
+		gui.hide(menuName)
+		gui.animateMenu('sideMainMenu', 'showSidebar')
+		
+registerSideMenuSwitch('switchToBuildingMenu', 'buildMenu')
+registerSideMenuSwitch('switchToSmallVehiclesMenu', 'buildSmallVehicles')
 	
 @on('GUI_AnimationComplete', menuName='sideMainMenu')
 def swapMenus(eventParams):
+	global orr_activeSideMenu
+	if orr_activeSideMenu == 'sideMainMenu':
+		return
 	gui.hide('sideMainMenu')
+	gui.show(orr_activeSideMenu)
+	gui.animateMenu(orr_activeSideMenu, 'showSidebar')
+
+@on('GUI_Click', action="switchToMain")
+def returnToMainSidebar(eventParams):
+	global orr_activeSideMenu
+	gui.animateMenu(orr_activeSideMenu, 'hideSidebar')
+	orr_activeSideMenu = 'sideMainMenu'
