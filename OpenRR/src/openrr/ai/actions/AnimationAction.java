d76b8260c5b0b425eb1b978d65053332a11a4562
Bart van Blokland <bartiver@stud.ntnu.no>
2014-11-14 15:47:35 +0100

    Implemented teleport rock raider action

---
+++ OpenRR/src/openrr/ai/actions/AnimationAction.java
@@ -0,0 +1,69 @@
+package openrr.ai.actions;
+
+import orre.ai.tasks.Action;
+import orre.ai.tasks.TaskRequest;
+import orre.animation.AnimationType;
+import orre.gameWorld.core.GameWorld;
+import orre.gameWorld.core.Message;
+import orre.gameWorld.core.MessageHandler;
+import orre.gameWorld.core.MessageType;
+import orre.gameWorld.core.PropertyDataType;
+import orre.geom.mesh.Mesh3D;
+
+public class AnimationAction extends Action implements MessageHandler {
+	private boolean isFinished = false;
+	
+	private final Mesh3D rootNode;
+	private final GameWorld world;
+	private final AnimationType animationType;
+	
+	public static AnimationAction plan(TaskRequest request, AnimationType type, GameWorld world) {
+		Mesh3D rootNode = (Mesh3D) world.requestPropertyData(request.targetID, PropertyDataType.APPEARANCE, null, Mesh3D.class);
+		return new AnimationAction(rootNode, type, world);
+	}
+	
+	private AnimationAction(Mesh3D rootNode, AnimationType type, GameWorld world) {
+		this.rootNode = rootNode;
+		this.animationType = type;
+		this.world = world;
+	}
+
+	@Override
+	public boolean isExecutionPossible() {
+		return true; //Always possible
+	}
+
+	@Override
+	public void update() {
+		
+	}
+
+	@Override
+	public boolean isFinished() {
+		return isFinished;
+	}
+
+	@Override
+	public double getCost() {
+		return 0; //0: not relevant for cost calculations
+	}
+
+	@Override
+	public void handleMessage(Message<?> message) {
+		if(message.type == MessageType.ANIMATION_ENDED) {
+			this.isFinished = true;
+		}
+	}
+
+	@Override
+	public void start() {
+		world.services.animationService.applyAnimation(animationType, rootNode);
+		world.addMessageListener(MessageType.ANIMATION_ENDED, this);
+	}
+
+	@Override
+	public void end() {
+		
+	}
+
+}
--- OpenRR/src/openrr/ai/tasks/TeleportRockRaiderTask.java
+++ OpenRR/src/openrr/ai/tasks/TeleportRockRaiderTask.java
@@ -1,9 +1,15 @@
 package openrr.ai.tasks;
 
 import openrr.ai.TaskType;
+import openrr.ai.actions.AnimationAction;
+import orre.ai.tasks.Action;
 import orre.ai.tasks.Assignment;
+import orre.ai.tasks.Plan;
+import orre.ai.tasks.Task;
 import orre.ai.tasks.TaskMaster;
 import orre.ai.tasks.TaskRequest;
+import orre.animation.AnimationType;
+import orre.gameWorld.core.GameWorld;
 
 public class TeleportRockRaiderTask extends ORRTask {
 
@@ -12,9 +18,9 @@ public class TeleportRockRaiderTask extends ORRTask {
 	}
 
 	@Override
-	public Assignment plan(TaskRequest request, TaskMaster taskMaster) {
-		System.out.println("Planning..");
-		return null;
+	public Assignment plan(TaskRequest request, TaskMaster taskMaster, GameWorld world) {
+		AnimationAction action = AnimationAction.plan(request, AnimationType.teleportRockRaider, world);
+		return new Assignment(new Task[]{this}, new Plan(new Action[]{action}));
 	}
 
 }
